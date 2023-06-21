0a8a9e5c0a90815b858390ade215d72f990e0729
CampbellBiology <urmapw@naver.com>
2023-06-21 18:47:37 +0900

    0621_1

--- src/main/java/CocktailOfTheDay/project/Controller/LiquorItemController.java
+++ src/main/java/CocktailOfTheDay/project/Controller/LiquorItemController.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 @RestController
 public class LiquorItemController {
 
+    //아이템 리스트 만들 때
     @RequestMapping("/api/item")
     public ArrayList<ItemResponse> liquorItem(@RequestBody HashMap<String, Object> _userInfo) throws SQLException {
 
@@ -58,12 +59,13 @@ public class LiquorItemController {
             //sql문 세팅
             String sql = "select b.ingredient_name, b.img_path from my_keep a\n" +
                     "            left join ingredient b\n" +
-                    "            on a.ingredient_index = b.ingredient_index where user_id = '" + user_id + "';";
+                    "            on a.ingredient_name = b.ingredient_name where user_id = '" + user_id + "';";
+
+            //String sql = "select ingredient_name, img_path from my_keep where user_id = '" + user_id + "';";
 
 
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
-
 
 
             //재료 여러 개 담기
@@ -108,5 +110,205 @@ public class LiquorItemController {
         return result;
     }
 
+
+
+    //item 삭제할 때
+    @RequestMapping("/api/itemDelete")
+    public int itemDelete(@RequestBody HashMap<String, Object> _info) throws SQLException {
+
+
+
+        System.out.println("ItemDelete user_id : " + _info.get("user_id"));
+        System.out.println("ItemDelete selectedIngName : " + _info.get("ingredient_name").toString());
+
+        String user_id = _info.get("user_id").toString();
+        String selectedIngName = _info.get("ingredient_name").toString();
+
+
+
+        //결과로 int result 반환
+        int result = 0;
+
+        //DBConn 초기회
+        DBConn DBconn;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+
+        try {
+
+            //DB 연결
+            DBconn = new DBConn();
+            conn = DBconn.connect();
+
+
+            //sql문 세팅
+            String sql = "delete from my_keep where ingredient_name = '" + selectedIngName + "' and user_id = '" + user_id + "';";
+
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+
+            result = 1;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = 99;
+
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e3) {
+                e3.printStackTrace();
+            }
+
+        }
+
+        return result;
+    }
+
+
+    //item 추가할 때
+    @RequestMapping("/api/itemAdd")
+    public int itemAdd(@RequestBody HashMap<String, Object> _info) throws SQLException {
+
+
+        System.out.println("ItemAdd user_id : " + _info.get("user_id"));
+        System.out.println("ItemAdd selectedIngName : " + _info.get("ingredient_name").toString());
+
+        String user_id = _info.get("user_id").toString();
+        String selectedIngName = _info.get("ingredient_name").toString();
+
+
+
+        //결과로 int result 반환
+        int result = 0;
+
+        //DBConn 초기회
+        DBConn DBconn;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+
+        try {
+
+            //DB 연결
+            DBconn = new DBConn();
+            conn = DBconn.connect();
+
+
+            //sql문 세팅
+            //INSERT INTO my_keep (user_id, ingredient_name) VALUES ('1', '토닉워터');
+            // String sql = "insert chat (couple_index, email, sender, message, timestamp2) values (?,?,?,?,?);";
+            String sql = "insert into my_keep (user_id, ingredient_name) values (?,?);";
+
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, user_id);
+            pstmt.setString(2, selectedIngName);
+
+            pstmt.executeUpdate();
+
+            result = 1;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = 99;
+
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e3) {
+                e3.printStackTrace();
+            }
+
+        }
+
+        return result;
+    }
+
+
+    //모든 재료 가져오기
+    @RequestMapping("/api/getAllIngredient")
+    public ArrayList<ItemResponse> itemDelete() throws SQLException {
+
+        //재료를 넣어서 반환할 ArrayList
+        ArrayList<ItemResponse> resultList = new ArrayList<>();
+
+        //DBConn 초기회
+        DBConn DBconn;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+
+        try {
+
+            //DB 연결
+            DBconn = new DBConn();
+            conn = DBconn.connect();
+
+
+            //sql문 세팅
+            String sql = "select ingredient_name, img_path from ingredient;";
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+
+            //재료 다 담기
+            while (rs.next()) {
+                //ArrayList에 담을 클래스 생성
+                ItemResponse Cabinet = new ItemResponse();
+
+                //클레스에 멤버 세팅
+                Cabinet.setIngredient_name(rs.getString(1));
+                Cabinet.setImg_path(rs.getString(2));
+
+                //ArrayList에 add하기
+                resultList.add(Cabinet);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e3) {
+                e3.printStackTrace();
+            }
+
+        }
+        return resultList;
+
+    }
 
 }
